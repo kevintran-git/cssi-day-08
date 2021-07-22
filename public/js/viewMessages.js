2a6571fb5d44c9a3c1e806c6033f8cb109fc112b
Kevin <kinzerfest@gmail.com>
2021-07-22 18:40:03 +0000

    Added cryptography!

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,13 +1,39 @@
+let attempts = 0;
+
 const getMessages = () => {
+    const input = document.querySelector("#passcode").value;
+    const hashedPasscode = CryptoJS.SHA3(input).toString();
     const messagesRef = firebase.database().ref();
-        messagesRef.on('value', (snapshot) => {
+
+    document.querySelector("#message").innerHTML = "";
+
+    messagesRef.on('value', (snapshot) => {
         const data = snapshot.val();
-        console.log(data);
-        for(const entry in data){
-            console.log(entry);
+
+        let found = false;
+        for (let key in data) {
+            console.log(data[key].passcode, data[key].message);
+            if (data[key].passcode === hashedPasscode) {
+                document.querySelector("#message").innerHTML += CryptoJS.AES.decrypt(data[key].message, hashedPasscode).toString(CryptoJS.enc.Utf8) + "<br>";
+                found = true;
+                attempts = 0;
+            }
         }
-        const input = document.querySelector("#passcode").value;
-        console.log(input);
+        if (found === false) {
+            if (attempts >= 5) {
+                document.querySelector("#message").innerHTML = `Please wait before trying again.`;
+                document.querySelector("#viewMsg").classList.add("is-hidden");
+                setTimeout(function () {
+                    document.querySelector("#viewMsg").classList.remove("is-hidden");
+                    attempts = 0;
+                    document.querySelector("#message").innerHTML = `You can try again now.`;
+                }, 3000);
+            } else {
+                attempts++;
+                document.querySelector("#message").innerHTML = `Sorry. There are no matching messages with that message. You have ${5 - attempts} attempts left.`;
+            }
+
+        }
     });
 }
 
---
+++ public/js/writeMessage.js
@@ -0,0 +1,14 @@
+const onSubmit = () => {
+    const passcodeInput = document.querySelector("#passcode").value;
+    const messageInput = document.querySelector("#message").value;
+    const hashedPasscode = CryptoJS.SHA3(passcodeInput).toString();
+
+    const payload = {
+        passcode: hashedPasscode,
+        message: CryptoJS.AES.encrypt(messageInput, hashedPasscode).toString()
+    }
+
+    console.log(CryptoJS.AES.decrypt(payload.message, hashedPasscode).toString(CryptoJS.enc.Utf8));
+    firebase.database().ref().push(payload);
+
+}
